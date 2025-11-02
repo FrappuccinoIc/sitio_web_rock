7a206e241a2011be5ca923a4329542afe372e411
Frappuccinoic <arseeicore@gmail.com>
2025-11-02 14:07:43 -0300

    Uso de procesador de contexto

--- core/processors.py
+++ core/processors.py
@@ -2,7 +2,7 @@
 
 def ctx_dict(req):
     ctx = {
-        "NOMBRE_PÁGINA": "Fans del Prog",
+        "NOMBRE_PAGINA": "Fans del Prog",
         "LINK_GITHUB": "https://github.com/FrappuccinoIc/sitio_web_rock"
     }
     return ctx
