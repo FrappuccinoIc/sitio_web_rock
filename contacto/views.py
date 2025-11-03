8db4b798dabdd520f2c1dc334ccfd3e108091956
Frappuccinoic <arseeicore@gmail.com>
2025-11-02 21:23:10 -0300

    Verificación de CSRF token

--- contacto/views.py
+++ contacto/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.urls import reverse
 from .forms import ContactForm
 
 def contacto(req):
-    contact_form = ContactForm
+    contact_form = ContactForm()
     if req.method == "POST":
-        contact_form = contact_form(data = req.POST)
+        contact_form = ContactForm(data = req.POST)
         if contact_form.is_valid():
             # get de un POST siempre debe conseguir una tupla al menos. Ya que nosotros queremos solo un valor, el segundo valor estará vacío.
             name = req.POST.get('name', '')
             email = req.POST.get('email', '')
             content = req.POST.get('content', '')
             return redirect(reverse('contacto') + '?ok')
+
     return render(req, 'contacto/contacto.html', {"form": contact_form})
