a44e8eeb6436c9a855c27e8ece5fbc88bfcfd31f
Frappuccinoic <arseeicore@gmail.com>
2025-11-16 20:17:25 -0300

    Enlazamiento de modelo usuario con Usuario del sistema

--- publicaciones/models.py
+++ publicaciones/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Usuario(models.Model):
     username = models.CharField(max_length=40, verbose_name="Nombre de Usuario")
     descripcion = models.TextField(verbose_name = "Descripción")
     imagen = models.ImageField(upload_to="projects", verbose_name="Perfil")
+    account = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Cuenta relacionada")
 
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Última vez actualizado")
