efd79f8c32d90984d3f647d838fa55137be7f39e
Frappuccinoic <arseeicore@gmail.com>
2025-12-01 03:31:59 -0300

    Falta cambiar valores de mailtrap

--- fansdelprog/settings.py
+++ fansdelprog/settings.py
@@ -124,3 +124,13 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 
 LOGIN_URL = '/admin/login/'
+
+# Mailtrap SMTP
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "sandbox.smtp.mailtrap.io"     # o el HOST que te de Mailtrap
+EMAIL_HOST_USER = "4d0b88d60d8ec8"
+EMAIL_HOST_PASSWORD = "****0369"
+EMAIL_PORT = 2525
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = "no-responder@fansdelprog.com"
